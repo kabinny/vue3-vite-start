49d06224584d4de3a5d8a8f15c04df3680ab7fd0
kabinny <kabinny@gmail.com>
2023-04-16 20:25:12 +0900

    텍스트 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,13 @@
-<!-- composition API -->
 <script setup>
-import HelloWorld from '@components/HelloWorld.vue'
+let text = '안녕 Vue!'
 </script>
 
-<!-- option API -->
-<!-- <script>
-import HelloWorld from '@components/HelloWorld.vue'
-export default {
-  components: { HelloWorld },
-}
-</script> -->
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ text }}</h1>
+  <p v-text="text"></p>
+  <p v-pre>
+    {{ escape }}
+  </p>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
