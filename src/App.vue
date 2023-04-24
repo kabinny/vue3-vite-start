9f91b35cac40558838bfa77c83ee9b97bbc319b0
kabinny <kabinny@gmail.com>
2023-04-24 21:39:49 +0900

    Methods and Data

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
-<!-- vue2 -->
 <script>
-export default {
-  data() {
-    let TextData = 'Hello'
-    return {
-      TextData,
-    }
-  },
-}
-</script>
-<!-- vue3 -->
-<script>
+// Vue3
 import { ref } from 'vue'
 export default {
   setup() {
-    let TextData2 = ref('Hello2')
+    let NumData = ref(0)
+
+    const PlusMethod = () => {
+      // this를 사용하지 않기 때문에 화살표 함수 OK
+      // 반응형 데이터를 위해 ref를 썼는데,
+      // ref는 내부적으로 객체처럼 값을 다루기 때문에 .value로 접근
+      NumData.value++
+    }
+
     return {
-      TextData2,
+      NumData,
+      PlusMethod,
     }
   },
 }
+
+/* Vue2
+export default {
+  data() {
+    let NumData = 0
+    return {
+      NumData,
+    }
+  },
+  methods: {
+    PlusMethod: function () {
+      // this에 접근해야 하기 때문에 화살표 함수 사용 X
+      this.NumData++
+    },
+  },
+}
+*/
 </script>
 
-<template></template>
+<template>
+  <button @click="PlusMethod">{{ NumData }}</button>
+</template>
 
 <style scoped></style>
