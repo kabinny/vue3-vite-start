fcaacbb73e2b928b275c67fbda208a0fd8271e38
kabinny <kabinny@gmail.com>
2023-04-25 21:07:32 +0900

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <script setup>
 import { ref } from 'vue'
 
-let text = ref('원본 값')
+let message = ref('')
+let messageLazy = ref('')
 
-const textInput = (event) => {
-  console.log(event)
-
-  text.value = event.target.value
-}
+let multiLineMessage = ref('')
+let checked = ref(false)
+let selected = ref('')
+let checkedNames = ref([])
 </script>
 
 <template>
-  <input :value="text" type="text" @input="textInput" />
-  <p>{{ text }}</p>
+  <!-- on input -->
+  <input v-model="message" placeholder="여기를 수정해보세요" type="text" />
+  <p>message: {{ message }}</p>
+  <!-- on change -->
+  <input v-model.lazy="messageLazy" type="text" />
+  <p>message: {{ messageLazy }}</p>
+
+  <hr />
+
+  <textarea
+    v-model="multiLineMessage"
+    placeholder="여러줄을 입력해보세요"
+  ></textarea>
+  <p style="white-space: pre-line">{{ multiLineMessage }}</p>
+  <hr />
+
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+  <hr />
+
+  <select v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>Selected: {{ selected }}</span>
+  <hr />
+
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+  <label for="jack">Jack</label>
+  <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+  <label for="john">John</label>
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+  <label for="mike">Mike</label>
+  <br />
+  <span>Checked names: {{ checkedNames }}</span>
 </template>
 
 <style scoped></style>
