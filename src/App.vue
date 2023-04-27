44b50c9e4f5c5ac090aab1fc4641add9adb7d28e
kabinny <kabinny@gmail.com>
2023-04-27 20:57:08 +0900

    computed vs methods

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,48 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { reactive, computed } from 'vue'
 
-const count = ref(0)
-const calculated = computed({
-  get: () => count.value + 1,
-  set: (val) => {
-    count.value = val
-  },
+const objData = reactive({
+  panda: 0,
+  coding: 10,
+  temp: 10,
 })
-const computedSetter = () => {
-  calculated.value = 10
+
+const plusPandaComputed = computed(() => {
+  console.log('1. plusPandaComputed')
+  return objData.panda + objData.temp
+})
+
+const plusCodingComputed = computed(() => {
+  console.log('2. plusCodingComputed')
+  return objData.coding + objData.temp
+})
+
+const plusPandaMethod = () => {
+  console.log('3. plusPandaMethod')
+  return objData.panda + objData.temp
+}
+
+const plusCodingMethod = () => {
+  console.log('4. plusCodingMethod')
+  return objData.coding + objData.temp
+}
+
+const plusPanda = () => {
+  objData.panda = objData.panda + 10
+}
+
+const plusCoding = () => {
+  objData.coding = objData.coding + 10
 }
 </script>
 
 <template>
-  <div>count: {{ count }}</div>
-  <div>calculated: {{ calculated }}</div>
-
-  <button @click="computedSetter">click!</button>
+  <p>plusPandaComputed: {{ plusPandaComputed }}</p>
+  <p>plusCodingComputed: {{ plusCodingComputed }}</p>
+  <p>plusPandaMethod: {{ plusPandaMethod() }}</p>
+  <p>plusCodingMethodL {{ plusCodingMethod() }}</p>
+  <button @click="plusPanda">plusPanda</button>
+  <button @click="plusCoding">plusCoding</button>
 </template>
 
 <style scoped></style>
