8d9b0b6c674c8d4b1b4e2c1a7795c96794667e57
kabinny <kabinny@gmail.com>
2023-04-30 13:07:18 +0900

    watchEffect - options

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <script setup>
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, watchPostEffect, watchSyncEffect } from 'vue'
 
 const count = ref(0)
 const IncreaseCount = () => {
   count.value++
 }
 
-const stop = watchEffect(() => {
-  console.log(count.value)
-  // 내부에서 호출해도 됨
-  // if (count.value > 10) stop()
+watchEffect(
+  () => {
+    console.log('pre', count.value)
+  },
+  {
+    onTrack(e) {
+      console.log('on Track', e)
+    },
+    onTrigger(e) {
+      console.log('on Trigger', e)
+    },
+    flush: 'pre',
+    // flush: 'post',
+    // flush: 'sync',
+  }
+)
+
+// 가장 마지막에 동작
+watchPostEffect(() => {
+  console.log('post', count.value)
 })
 
-const stopMethod = () => {
-  stop()
-}
+watchSyncEffect(() => {
+  console.log('sync', count.value)
+})
 </script>
 
 <template>
   <p>원본 값: {{ count }}</p>
   <button @click="IncreaseCount">+1</button>
-  <button @click="stopMethod">stop!</button>
-  <!-- 함수로 감싸지 않고 바로 호출해도 됨 -->
-  <button @click="stop()">stop!!</button>
 </template>
 
 <style></style>
