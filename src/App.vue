209f733934f87cc0fb2548ecb16d13dbf74a2ff0
kabinny <kabinny@gmail.com>
2023-04-30 12:13:50 +0900

    vue3 - watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
-<script>
-export default {
-  data() {
-    let count = 0
-    let watchCount = 2
-    return {
-      count,
-      watchCount,
-    }
-  },
-  methods: {
-    IncreaseCount: function () {
-      this.count++
-    },
-  },
-  computed: {
-    getDouble: function () {
-      return this.count * 2
-    },
-  },
-  watch: {
-    count: function (newValue) {
-      this.watchCount = newValue * 2
-    },
-  },
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const count = ref(0)
+const IncreaseCount = () => {
+  count.value++
 }
+
+watchEffect(() => {
+  // watchEffect 안에서 쓰인 데이터가 바뀔 때마다 알아서 실행됨
+  console.log(count.value)
+})
 </script>
 
 <template>
   <p>원본 값: {{ count }}</p>
-  <p>computed: {{ getDouble }}</p>
-  <p>watch: {{ watchCount }}</p>
   <button @click="IncreaseCount">+1</button>
 </template>
 
