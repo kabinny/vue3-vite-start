85dee4983df6e3ae66d33ed6a67ed51dde63855a
kabinny <kabinny@gmail.com>
2023-05-02 21:42:36 +0900

    props - 모든 데이터 타입 전달

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
+// 1. 모든 데이터 타입을 전달할 수 있는가
+// 2. object, boolean 특별한 사용법
+// 3. props 유효성 검사
+// 4. function을 props로 보낼 때 유의해야 할 사항
 import HelloWorld from './components/HelloWorld.vue'
 
-const text = 'Hello!!!'
-const AlertMethod = () => {
-  alert(text)
+let StringData = '안녕'
+let NumberData = 123
+let ArrayData = ['a', 'b', 'c']
+let BooleanData = true
+let objectData = {
+  foo: 'hello',
+  bar: 'vue!',
 }
 </script>
 
 <template>
-  <HelloWorld @click="AlertMethod" id="안녕뷰" />
+  <HelloWorld :propsData="StringData" />
+  <HelloWorld :propsData="NumberData" />
+  <HelloWorld :propsData="ArrayData" />
+  <HelloWorld :propsData="BooleanData" />
+  <HelloWorld :propsData="objectData" />
 </template>
 
 <style></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,12 @@
-<script>
-import { useAttrs } from 'vue'
-
-export default {
-  inheritAttrs: false,
-}
-</script>
-
 <script setup>
-import { useAttrs } from 'vue'
+// import { defineProps } from 'vue'
 
-const attrs = useAttrs()
-console.log(attrs)
+const props = defineProps(['propsData'])
 </script>
 
 <template>
-  <button>No Attrs</button>
-  <button :id="attrs.id" @click="attrs.onClick">HelloWorldVue!</button>
-  <button>No Attrs</button>
+  <div>{{ props.propsData }}</div>
+  <hr />
 </template>
 
 <style scoped></style>
