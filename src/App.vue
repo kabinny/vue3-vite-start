a8e0acb7f928f7d3514a395ae4640fe252cde443
kabinny <kabinny@gmail.com>
2023-05-30 22:07:53 +0900

    NavBar 구현하기

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import NavBar from './components/NavBar.vue'
+import MainComponent from './components/MainComponent.vue'
+import ListComponent from './components/ListComponent.vue'
+import UploadComponent from './components/UploadComponent.vue'
 
-<template></template>
+const category = ref('Main')
+
+const setCategory = (data) => {
+  category.value = data
+}
+
+const ComponentList = {
+  Main: MainComponent,
+  List: ListComponent,
+  Upload: UploadComponent,
+}
+</script>
+
+<template>
+  <NavBar :category="category" @setCategory="setCategory" />
+  <component :is="ComponentList[category]"></component>
+</template>
 
 <style></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  category: String,
+})
+
+const emits = defineEmits(['setCategory'])
+
+const setCategoryHandler = (element) => {
+  if (props.category === element) return
+  emits('setCategory', element)
+}
+
+const setClassName = (element) => {
+  return computed(() => {
+    return element === props.category ? 'active' : null
+  })
+}
+</script>
+
+<template>
+  <ul>
+    <li :class="setClassName('Main').value" @click="setCategoryHandler('Main')">
+      Main
+    </li>
+    <li :class="setClassName('List').value" @click="setCategoryHandler('List')">
+      List
+    </li>
+    <li
+      :class="setClassName('Upload').value"
+      @click="setCategoryHandler('Upload')"
+    >
+      Upload
+    </li>
+  </ul>
+</template>
+
+<style>
+ul {
+  padding: 0;
+  list-style: none;
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+}
+
+.active {
+  cursor: default;
+  font-weight: bold;
+}
+
+li {
+  margin-left: 1rem;
+  cursor: pointer;
+  user-select: none;
+}
+
+li:nth-of-type(1) {
+  margin: 0;
+}
+</style>
