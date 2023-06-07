9d83d340f0230c986fbc43b16bdbbc9bcb7f47da
kabinny <kabinny@gmail.com>
2023-06-07 22:12:41 +0900

    routing 관리 - 동적 라우트에 props 사용

---
+++ src/components/DetailComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(['id'])
+</script>
+
+<template>
+  <h1>Detail Component</h1>
+  <hr />
+  {{ props.id }}
+</template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainComponent from './components/MainComponent.vue'
 import ListComponent from './components/ListComponent.vue'
 import UploadComponent from './components/UploadComponent.vue'
+import DetailComponent from './components/DetailComponent.vue'
 
 const ErrorPage = {
   template: `<div><h3>404 Page</h3></div>`,
@@ -11,6 +12,7 @@ const routes = [
   { path: '/', name: 'Main', component: MainComponent, props: { foo: 'bar' } },
   { path: '/list', name: 'List', component: ListComponent, props: true },
   { path: '/upload', name: 'Upload', component: UploadComponent },
+  { path: '/post/:id', component: DetailComponent, props: true },
   // { path: '/:pathMatch(.*)', name: 'No-match', redirect: '/' },
   { path: '/:pathMatch(.*)', name: 'No-match', component: ErrorPage },
 ]
